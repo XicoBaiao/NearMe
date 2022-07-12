bdc523b6bde0f2410f5e4095defe855645765c3c
XicoBaiao <baiao_maxima@outlook.com>
2022-07-13 00:55:15 +0100

    first commit

---
+++ NearMe/NearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeApp.swift
+//  NearMe
+//
+//  Created by Baiao, Francisco Fonseca on 13/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct NearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
